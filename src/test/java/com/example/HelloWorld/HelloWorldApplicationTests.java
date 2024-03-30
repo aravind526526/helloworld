55c1bb4cf89a6cd7c43c3291b7ab73cb16a9d72e
aravind <chepyalaaravind5262gmail.com>
2024-03-29 19:43:37 -0500

    asert

--- src/test/java/com/example/HelloWorld/HelloWorldApplicationTests.java
+++ src/test/java/com/example/HelloWorld/HelloWorldApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.HelloWorld;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class HelloWorldApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true,true);
 	}
 
 }
