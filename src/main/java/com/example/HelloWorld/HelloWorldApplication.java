d10f407a9df69639fe7ebfa00fbe2009f7c99f82
aravind <chepyalaaravind5262gmail.com>
2024-04-11 14:39:26 -0500

    added docker file

--- src/main/java/com/example/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorld/HelloWorldApplication.java
@@ -3,11 +3,20 @@ package com.example.HelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.log4j.*;
+
 @SpringBootApplication
 public class HelloWorldApplication {
-
+	private static org.apache.log4j.Logger log = Logger.getLogger(HelloWorldApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+		log.setLevel(Level.WARN);
+		log.trace("Trace Message!");
+		log.debug("Debug Message!");
+		log.info("Info Message!");
+		log.warn("Warn Message!");
+		log.error("Error Message!");
+		log.fatal("Fatal Message!");
 	}
 
 }
